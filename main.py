dbdf4666bac332df230bc053adf743a694dd0f45
jane-mykh <emykh.ca@gmail.com>
2024-10-08 15:28:27 -0400

    init

---
+++ main.py
@@ -0,0 +1,32 @@
+import secrets
+import string
+
+
+print('''Greetings!
+This is a password generator.
+It can generate a password from 8 to 20 symbols.''')
+number_of_symbols = int(input("Please enter number from 8 to 20 symbols for the password: "))
+while number_of_symbols < 8 or number_of_symbols > 20:
+    number_of_symbols = int(input("Please enter again number from 8 to 20 symbols for the password: "))
+    if number_of_symbols < 8 or number_of_symbols > 20:
+        print("The number can't be lesser than 8 or greater than 20")
+    else:
+        print(f"Generating a password for {number_of_symbols} symbols")
+
+
+def generate_random(length):
+    symbols = ("!@#$%^&*")
+    alphabet = string.ascii_letters + string.digits + symbols
+    j = 1
+    while True:      
+        password = ''.join(secrets.choice(alphabet) for i in range(length))
+        if (any(c.islower() for c in password)
+            and any(c.isupper() for c in password)
+            and any(c.isdigit() for c in password)
+            and any(c in symbols for c in password)):
+            break
+        j+=1
+        print(j)
+
+    return password
+print(generate_random(number_of_symbols))
